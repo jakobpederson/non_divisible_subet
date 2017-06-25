38620c996575e329fdde65f73e56ec52534f2a20
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-25 11:23:17 -0500

    wip

--- non_divisible_subset.py
+++ non_divisible_subset.py
@@ -2,5 +2,12 @@
 
 class NonDivisbileSubset():
 
-    def get_subset(self):
-        return('hi')
+    def get_subset(self, n, k, arr):
+        new_arr = set(arr)
+        return(
+            len(
+                set(
+                    [x + y for x in arr for y in new_arr if (x + y) % k != 0 and x != y]
+                )
+            )
+        )
--- test_non_divisible_subset.py
+++ test_non_divisible_subset.py
@@ -8,4 +8,9 @@ class NonDivisibleSubsetTest(unittest.TestCase):
         self.client = non_divisible_subset.NonDivisbileSubset()
 
     def test_x(self):
-        self.fail(self.client.get_subset())
+        n = 4
+        k = 3
+        arr = [1, 7, 2, 4]
+        result = self.client.get_subset(n, k, arr)
+        self.assertEqual(3, result)
+        self.fail(self.client.get_subset(n, k, arr))
