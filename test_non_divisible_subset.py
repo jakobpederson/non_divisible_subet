f815472afe1b2e850037681441447055be094317
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-26 08:09:31 -0500

    wip

--- test_non_divisible_subset.py
+++ test_non_divisible_subset.py
@@ -8,22 +8,11 @@ class NonDivisibleSubsetTest(unittest.TestCase):
         self.client = non_divisible_subset.NonDivisibleSubset()
 
     def test_get_subset_returns_correct_value(self):
-        n = 4
-        k = 3
-        arr = [1, 7, 2, 4]
-        result = self.client.get_subset(n, k, arr)
-        self.assertEqual(3, result)
-        n2 = 5
-        k2 = 5
-        arr2 = [2, 7, 12, 17, 22]
-        result2 = self.client.get_subset(n2, k2, arr2)
-        self.assertEqual(5, result2)
-        n3 = 6
-        k3 = 9
-        arr3 = [422346306, 940894801, 696810740, 862741861, 85835055, 313720373]
-        result3 = self.client.get_subset(n3, k3, arr3)
-        self.assertEqual(5, result3)
+        self.assertEqual(3,  self.client.get_subset(4, 3, [1, 7, 2, 4]))
+        self.assertEqual(5, self.client.get_subset(5, 5, [2, 7, 12, 17, 22]))
+        self.assertEqual(
+            5, self.client.get_subset(6, 9, [422346306, 940894801, 696810740, 862741861, 85835055, 313720373])
+        )
         with open('test_data.txt') as f:
             lines = [int(x) for x in f.read().strip().split(' ')]
-        result4 = self.client.get_subset(100000, 100, lines)
-        self.assertEqual(49747, result4)
+        self.assertEqual(49747, self.client.get_subset(100000, 100, lines))
