2ad10353a4dc5ad30ce1cd5d129f0adccf95ed37
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-26 08:00:22 -0500

    style

--- non_divisible_subset.py
+++ non_divisible_subset.py
@@ -1,17 +1,16 @@
-from itertools import combinations
 
 
 class NonDivisibleSubset():
 
     def get_subset(self, n, k, arr):
         data = [x % k for x in arr]
-        return sum([(max(data.count(i), data.count(k - i)) if i != 0 and i * 2 != k else min(1, data.count(i))) for i in range(int(k/2) + 1)])
+        return sum(self.subset_size(k, data))
 
-        # for i in range(int(k/2) + 1):
-        #     print(i)
-        #     if i != 0 and i * 2 != k:
-        #         count += max(data.count(i), data.count(k - i))
-        #         print(count)
-        #     else:
-        #         count += min(1, data.count(i))
-        # return count
+    def subset_size(self, k, data):
+        return [
+            (
+                max(data.count(i), data.count(k - i)) if i != 0 and i * 2 != k else
+                min(1, data.count(i))
+            )
+            for i in range(int(k/2) + 1)
+        ]
--- test_non_divisible_subset.py
+++ test_non_divisible_subset.py
@@ -7,11 +7,10 @@ class NonDivisibleSubsetTest(unittest.TestCase):
     def setUp(self):
         self.client = non_divisible_subset.NonDivisibleSubset()
 
-    def test_x(self):
+    def test_get_subset_returns_correct_value(self):
         n = 4
         k = 3
         arr = [1, 7, 2, 4]
-        # result = next(self.client.get_subset(n, k, arr))
         result = self.client.get_subset(n, k, arr)
         self.assertEqual(3, result)
         n2 = 5
@@ -22,10 +21,9 @@ class NonDivisibleSubsetTest(unittest.TestCase):
         n3 = 6
         k3 = 9
         arr3 = [422346306, 940894801, 696810740, 862741861, 85835055, 313720373]
-        # result3 = next(self.client.get_subset(n3, k3, arr3))
         result3 = self.client.get_subset(n3, k3, arr3)
         self.assertEqual(5, result3)
-        # self.fail('x')
         with open('test_data.txt') as f:
             lines = [int(x) for x in f.read().strip().split(' ')]
-        self.fail(self.client.get_subset(100000, 100, lines))
+        result4 = self.client.get_subset(100000, 100, lines)
+        self.assertEqual(49747, result4)
