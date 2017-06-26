06f8222b4d7a4cfb51a70717766e02aceec23225
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-25 23:22:25 -0500

    wip

--- non_divisible_subset.py
+++ non_divisible_subset.py
@@ -4,9 +4,11 @@ from itertools import combinations
 class NonDivisbileSubset():
 
     def get_subset(self, n, k, arr):
-        result = [x for x in arr for y in arr if x != y and (x + y) % k != 0]
-        print(set(result), k)
-
-        new_result = [(x, arr[i], (x + arr[i]) % k) for i, x in enumerate(result) if i < n if (x + arr[i]) % k != 0]
-        print('new', new_result)
-        return(len(new_result))
+        while(n > 1):
+            comb = combinations(arr, n)
+            for comb_ in comb:
+                print(list(combinations(comb_, 2)))
+                if all((x + y) % k for (x, y) in combinations(comb_, 2)):
+                    return n
+            n = n - 1
+        return 0
