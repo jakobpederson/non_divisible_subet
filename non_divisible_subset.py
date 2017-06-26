3270011fbd6a8f18a25e35d679d02150619469ba
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-25 19:48:26 -0500

    passing test

--- non_divisible_subset.py
+++ non_divisible_subset.py
@@ -3,14 +3,12 @@
 class NonDivisbileSubset():
 
     def get_subset(self, n, k, arr):
-        # result = [x + y for i, x in enumerate(arr) for j, y in enumerate(arr) if (x + y) != 0 and i < j]
-        result = set([x + y for i, x in enumerate(arr) for j, y in enumerate(arr) if i < j and (x + y) % k != 0])
-
-        # result = set([x + y for x in arr for y in arr if (x + y) % k != 0 and x != y])
+        # result = set([(x, arr) for i, x in enumerate(arr) if i + 1 < len(arr) and (x + arr[i + 1]) / k != 0])
+        print(k)
+        result = set([x for x in arr for y in arr if x != y and (x + y) % k != 0])
         print(result)
         return(
             len(
                     result
-                    # [x + y for x in arr for y in arr if (x + y) % k != 0 and x != y]
             )
         )
